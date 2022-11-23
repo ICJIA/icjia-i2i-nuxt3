e7c348787a4d76d13e101dd133e12af1782415ed
cschweda <cschweda@gmail.com>
2022-11-23 09:49:29 -0600

    fix: ports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,15 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: "utf-16",
-      viewport: "initial-scale=1",
-      title: "My App",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "i2i | Institute to Innovate",
       meta: [
         // <meta name="description" content="My amazing site">
-        { name: "description", content: "My amazing site." },
+        {
+          name: "description",
+          content: "Investing in Public Safety by Investing in You.",
+        },
       ],
     },
   },
