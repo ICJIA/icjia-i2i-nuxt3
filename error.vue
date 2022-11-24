80cb5c2f68b88fd1984f43c51861315ca07e9a66
cschweda <cschweda@gmail.com>
2022-11-24 12:45:35 -0600

    fix: error layout

--- error.vue
+++ error.vue
@@ -1,10 +1,22 @@
 <template>
-  <div style="background: #eee; min-height: 100vh">
-    <v-container class="text-center" fill-height fluid>
+  <div>
+    <div>
+      <NuxtLink to="/">Home</NuxtLink> |
+      <NuxtLink to="/about/">About</NuxtLink> |
+      <NuxtLink to="/blog/">Blog</NuxtLink> |
+      <NuxtLink to="/sandbox/">Sandbox</NuxtLink>
+    </div>
+
+    <v-container
+      class="text-center"
+      fill-height
+      fluid
+      style="background: #eee; min-height: 100vh"
+    >
       <v-row align="center" justify="center"
         ><v-col>
           <h1 style="font-size: 80px">{{ errorObj.error.statusCode }}</h1>
-          <p>{{ errorObj.error.statusMessage }}.</p>
+          <p>{{ errorObj.error.statusMessage.split(":")[0] }}</p>
 
           <v-btn class="mt-8" @click="handleError">Go Home</v-btn>
         </v-col></v-row
