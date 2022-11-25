6ebce61472dc0b7337901615a87de9cab2e8c4bc
cschweda <cschweda@gmail.com>
2022-11-25 14:13:40 -0600

    fix: nav responsive

--- components/content/TheNav.vue
+++ components/content/TheNav.vue
@@ -1,20 +1,21 @@
 <template>
   <div>
-    <v-app-bar color="grey darken-1" app>
+    <v-app-bar color="grey darken-1" elevation="4">
       <template #prepend>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon class="hidden-md-and-up"></v-app-bar-nav-icon>
       </template>
 
       <v-app-bar-title>Title here</v-app-bar-title>
-      <v-btn to="/">Home</v-btn>
-      <v-btn to="/about">About</v-btn>
-      <v-btn to="/blog">Blog</v-btn>
-      <v-btn to="/redirect">404 redirect</v-btn>
-      <!-- <template #append>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </template> -->
+      <v-btn to="/" class="hidden-sm-and-down">Home</v-btn>
+      <v-btn to="/about" class="hidden-sm-and-down">About</v-btn>
+      <v-btn to="/blog" class="hidden-sm-and-down">Blog</v-btn>
+      <v-btn to="/redirect" class="hidden-sm-and-down">404</v-btn>
+
+      <template #append>
+        <v-btn icon class="hidden-md-and-up">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
     </v-app-bar>
   </div>
 </template>
