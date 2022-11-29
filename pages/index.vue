672a0dc9355ff9e8e031ab33cddd98258a343645
cschweda <cschweda@gmail.com>
2022-11-29 11:21:21 -0600

    fix: hydration mismatch w/no-ssr

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,29 @@
 <template>
-  <div>
-    <v-container fluid
-      ><v-row
-        ><v-col>
-          <h1>This is the home page</h1>
-          <Test></Test></v-col></v-row
-    ></v-container>
-    <!-- <h2>Display config (resize to see reactive changes)</h2>
-    {{ $vuetify.display }}<br /> -->
-    <!-- Screen xxl: {{ xxl }}<br />
+  <v-no-ssr>
+    <div>
+      <v-container fluid
+        ><v-row
+          ><v-col>
+            <h1>This is the home page</h1>
+            <Test></Test></v-col></v-row
+      ></v-container>
+      <h2>Display config (resize to see reactive changes)</h2>
+
+      <pre> {{ $vuetify.display }}</pre>
+
+      <br />
+      <!-- Screen xxl: {{ xxl }}<br />
     Screen xl: {{ xl }}<br />
     Screen lg: {{ lg }}<br />
     Screen md: {{ md }}<br />
     Screen sm: {{ sm }}<br />
     Screen xs: {{ xs }}<br /> -->
-    <h2>App config</h2>
-    {{ appConfig }}
-    <h2>Sitemap</h2>
-    Click to view <a href="/sitemap.xml">sitemap.xml</a>
-  </div>
+      <h2>App config</h2>
+      {{ appConfig }}
+      <h2>Sitemap</h2>
+      Click to view <a href="/sitemap.xml">sitemap.xml</a>
+    </div>
+  </v-no-ssr>
 </template>
 
 <script setup>
