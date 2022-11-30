fbf26910546acd0b0b2317eb56adb141618900f3
cschweda <cschweda@gmail.com>
2022-11-30 11:35:20 -0600

    fix: home

--- pages/index.vue
+++ pages/index.vue
@@ -6,18 +6,6 @@
           <h1>This is the home page</h1>
           <Test></Test></v-col></v-row
     ></v-container>
-    <v-no-ssr>
-      <h2>Breakpoint/Display config (client-only)</h2>
-
-      <pre> {{ $vuetify.display }}</pre>
-
-      <br />
-
-      <h2>App config</h2>
-      {{ appConfig }}
-      <h2>Sitemap</h2>
-      Click to view <a href="/sitemap.xml">sitemap.xml</a>
-    </v-no-ssr>
   </NuxtLayout>
 </template>
 
