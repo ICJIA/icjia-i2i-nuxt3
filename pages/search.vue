56c3cc496b0fe390a52bf218e28b8e7c250aad3c
cschweda <cschweda@gmail.com>
2022-12-04 13:06:06 -0600

    fix: search

--- pages/search.vue
+++ pages/search.vue
@@ -1,40 +1,47 @@
 <template>
   <div>
-    <h1>This is the search page</h1>
+    <v-container
+      ><v-row
+        ><v-col>
+          <h1>This is the search page</h1>
+          <h4>Search query</h4>
+          <pre v-if="query && query.length">    {{ query }}</pre>
+          <pre v-else><strong>Please enter search query</strong></pre>
 
-    <v-form class="pl-2 mt-4" style="margin-top: -15px">
-      <v-text-field
-        ref="textfield"
-        v-model="query"
-        clearable
-        autofocus
-        label="Search"
-        placeholder="Enter search term"
-        style="font-weight: 900"
-        @input="instantSearch"
-      />
-    </v-form>
-    <div v-if="result && query && query.length">
-      <h2>Search results:</h2>
-      Found: {{ result.length }}
-      <div v-if="result.length">
-        <v-card
-          v-for="(result, index) in result"
-          :key="`fuse-${index}`"
-          class="px-5 py-5 mx-5 my-10"
-          elevation="5"
-        >
-          <p>{{ result.item }}</p>
-        </v-card>
-      </div>
-    </div>
-    <h4>Search query</h4>
-    <pre v-if="query && query.length">    {{ query }}</pre>
-    <pre v-else><strong>Please enter search query</strong></pre>
-    <h4>Search options</h4>
-    <pre>    {{ options }}</pre>
-    <h4>Search index</h4>
-    <pre>    {{ searchIndex }}</pre>
+          <v-form class="pl-2 mt-4" style="margin-top: -15px">
+            <v-text-field
+              ref="textfield"
+              v-model="query"
+              clearable
+              autofocus
+              label="Search"
+              placeholder="Enter search term"
+              style="font-weight: 900"
+              @input="instantSearch"
+            />
+          </v-form>
+          <div v-if="result && query && query.length">
+            <h2>Search results:</h2>
+            Found: {{ result.length }}
+            <div v-if="result.length">
+              <v-card
+                v-for="(result, index) in result"
+                :key="`fuse-${index}`"
+                class="px-5 py-5 mx-5 my-10"
+                elevation="5"
+              >
+                <p>{{ result.item }}</p>
+              </v-card>
+            </div>
+          </div>
+
+          <h4>Search options</h4>
+          <pre>    {{ options }}</pre>
+          <h4>Search index</h4>
+          <pre>    {{ searchIndex }}</pre>
+        </v-col></v-row
+      ></v-container
+    >
   </div>
 </template>
 
