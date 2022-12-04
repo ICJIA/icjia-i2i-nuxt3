e5d97ad32ea06b72fef4b40b46b6a8b5c74a2cc6
cschweda <cschweda@gmail.com>
2022-12-04 11:11:28 -0600

    feat: scaffold out search page for future fuse.js search

--- pages/search.vue
+++ pages/search.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <h1>This is the search page</h1>
+
+    <h2>Search index (from /site.json):</h2>
+    {{ siteContent }}
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import siteContent from "../public/site.json";
+</script>
 
 <style lang="scss" scoped></style>
