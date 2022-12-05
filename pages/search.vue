0cc60746234a556d427d69e3586703fc8725d622
cschweda <cschweda@gmail.com>
2022-12-04 21:05:26 -0600

    fix: add focus to search

--- pages/search.vue
+++ pages/search.vue
@@ -4,27 +4,24 @@
       ><v-row
         ><v-col>
           <h1>This is the search page</h1>
-          <h4>Search query</h4>
-          <div v-if="query && query.length">
-            {{ query }}
-          </div>
-          <pre v-else><strong>Please enter search query</strong></pre>
 
           <v-form class="pl-2 mt-4" style="margin-top: -15px">
             <v-text-field
+              id="textfield"
               ref="textfield"
               v-model="query"
               clearable
-              autofocus
               label="Search"
               placeholder="Enter search term"
               style="font-weight: 900"
               @input="instantSearch"
             />
           </v-form>
-          <div v-if="result && query && query.length">
-            <h2>Search results:</h2>
-            Found: {{ result.length }}
+          <div v-if="result && query?.length">
+            <div class="text-center">
+              <h2>Search results:</h2>
+              Found: {{ result.length }}
+            </div>
             <div v-if="result.length">
               <v-card
                 v-for="(result, index) in result"
@@ -52,11 +49,24 @@
               </v-card>
             </div>
           </div>
-
-          <h4>Search options</h4>
-          <pre>    {{ options }}</pre>
-          <h4>Search index</h4>
-          <pre>    {{ searchIndex }}</pre>
+          <div class="text-center">
+            <v-btn v-if="!showIndex" @click.prevent="toggleIndex"
+              >DEBUG: Show search index<v-icon right
+                >mdi-chevron-down</v-icon
+              ></v-btn
+            >
+            <v-btn v-else @click.prevent="toggleIndex"
+              >DEBUG: Hide search index<v-icon right
+                >mdi-chevron-up</v-icon
+              ></v-btn
+            >
+            <div v-if="showIndex" class="text-left">
+              <h4>Search index</h4>
+              <pre style="text-left">    {{ searchIndex }}</pre>
+              <h4>Search options</h4>
+              <pre>    {{ options }}</pre>
+            </div>
+          </div>
         </v-col></v-row
       ></v-container
     >
@@ -86,7 +96,7 @@ const options = {
   ignoreFieldNorm: false,
   keys: ["title", "slug", "summary", "searchMeta", "rawText"],
 };
-const query = ref("Institute 2 Innovate");
+const query = ref("");
 
 const fuse = new Fuse(searchIndex, options);
 
@@ -96,11 +106,21 @@ const instantSearch = () => {
   result = fuse.search(query.value);
 };
 
+const showIndex = ref(false);
+const toggleIndex = () => {
+  showIndex.value = !showIndex.value;
+};
+
 const router = useRouter();
 const navigateTo = (item) => {
   console.log("navigateTo: ", item?.path);
   router.push({ path: item.path });
 };
+
+onMounted(() => {
+  const el = document.getElementById("textfield");
+  el.focus();
+});
 </script>
 
 <style lang="scss" scoped></style>
