fbb2571cbaca98837eda066abeab0b1109c3073a
cschweda <cschweda@gmail.com>
2022-12-05 11:22:49 -0600

    feat: sidebar accordion scaffold

--- components/content/TheSidebar.vue
+++ components/content/TheSidebar.vue
@@ -6,7 +6,7 @@
       temporary
       style="background: #fff"
     >
-      <v-list>
+      <!-- <v-list>
         <v-list-item prepend-icon="mdi-view-dashboard" to="/">Home</v-list-item>
         <v-list-item prepend-icon="mdi-view-dashboard" to="/about"
           >About</v-list-item
@@ -20,12 +20,33 @@
         <div class="pa-2">
           <v-btn block>LOGOUT </v-btn>
         </div>
-      </template>
+      </template> -->
+      <v-list v-model:opened="open">
+        <v-list-item prepend-icon="mdi-home" title="Home"></v-list-item>
+        <div>
+          <v-list-group value="Test">
+            <template #activator="{ props }">
+              <v-list-item v-bind="props" title="Test"></v-list-item>
+            </template>
+
+            <v-list-item
+              v-for="([title, icon], i) in admins"
+              :key="i"
+              :title="title"
+              :prepend-icon="icon"
+              :value="title"
+              style="margin-left: -35px"
+            ></v-list-item>
+          </v-list-group>
+        </div>
+      </v-list>
     </v-navigation-drawer>
   </div>
 </template>
 
 <script setup>
+const appConfig = useAppConfig();
+const navMenu = JSON.parse(JSON.stringify(appConfig.navMenu));
 const isMounted = ref(false);
 const drawer = ref(false);
 const altState = useNavToggle();
@@ -43,6 +64,18 @@ const click = () => {
 onMounted(() => {
   isMounted.value = true;
 });
+
+const open = ref(["Users"]);
+const admins = ref([
+  ["Management", "mdi-account-multiple-outline"],
+  ["Settings", "mdi-cog-outline"],
+]);
+const cruds = ref([
+  ["Create", "mdi-plus-outline"],
+  ["Read", "mdi-file-outline"],
+  ["Update", "mdi-update"],
+  ["Delete", "mdi-delete"],
+]);
 </script>
 
 <style lang="scss" scoped></style>
